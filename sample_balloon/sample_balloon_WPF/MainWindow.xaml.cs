cebeb61f105c83525ae8b123068277bb4ffbbc54
ksudoh <ksudoh@sciencepark.co.jp>
2019-12-24 17:45:44 +0900

    WPF（.NET Framework)でバルーンを表示するサンプルコードを作成。「前のバルーンが消えたら、次のバルーンを表示する」という実装。NuGetパッケージを使用した。

---
+++ sample_balloon/sample_balloon_WPF/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace sample_balloon_WPF
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private NotifyBalloon m_NotifyBalloon;
+        private TaskbarIcon m_TaskbarIcon;
+
+        private void OnBtnBalloon(object sender, RoutedEventArgs e)
+        {
+            m_NotifyBalloon.NotifyShow(DateTime.Now.ToString());
+        }
+
+        private void OnBtnCreateTaskTray(object sender, RoutedEventArgs e)
+        {
+            if (m_TaskbarIcon != null)
+            {
+                MessageBox.Show("Exist");
+                return;
+            }
+
+            m_TaskbarIcon = new TaskbarIcon();
+
+            using (var iconStream = Application.GetResourceStream(new Uri("pack://application:,,,/Resources/Icon1.ico", UriKind.RelativeOrAbsolute))?.Stream)
+            {
+                if (iconStream == null)
+                {
+                    throw new Exception($"Icon Read Fail");
+                }
+
+                m_TaskbarIcon.Icon = new Icon(iconStream);
+            }
+
+            m_NotifyBalloon = new NotifyBalloon(m_TaskbarIcon);
+        }
+    }
+}
---
+++ sample_balloon/sample_balloon_WPF/NotifyBalloon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace sample_balloon_WPF
+{
+    public class NotifyBalloon
+    {
+        public void NotifyShow(string message)
+        {
+            var item = new BalloonData
+            {
+                Message = message,
+                Title = string.Empty
+            };
+
+            m_ShowWaitBalloonData.Enqueue(item);
+
+            if (m_ShowWaitBalloonData.Count <= 1)
+            {
+                Application.Current.Dispatcher?.Invoke(() => m_TaskTray.ShowBalloonTip(item.Title, item.Message, BalloonIcon.None));
+            }
+        }
+
+        public NotifyBalloon(TaskbarIcon taskTray)
+        {
+            m_TaskTray = taskTray;
+            m_TaskTray.TrayBalloonTipClosed += OnBalloonTipClosed;
+        }
+
+
+        class BalloonData
+        {
+            public string Message;
+            public string Title;
+        }
+
+        /// <summary>
+        /// 表示待ちバルーンデータを入れるキュー。現在表示中のデータもキューの中に残し、バルーンを閉じたらキューから消す。
+        /// </summary>
+        private readonly ConcurrentQueue<BalloonData> m_ShowWaitBalloonData = new ConcurrentQueue<BalloonData>();
+
+        private readonly TaskbarIcon m_TaskTray;
+
+        public void OnBalloonTipClosed(object sender, RoutedEventArgs e)
+        {
+            //今閉じたバルーンの情報がキューに入っているので、削除。続けてキューにデータが残っている場合は、次を表示。
+            m_ShowWaitBalloonData.TryDequeue(out _);
+
+            if (m_ShowWaitBalloonData.TryPeek(out var item))
+            {
+                Application.Current.Dispatcher?.Invoke(() => m_TaskTray.ShowBalloonTip(item.Title, item.Message, BalloonIcon.None));
+            }
+        }
+    }
+}
